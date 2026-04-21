174a4c0c7d8e13bd0e2cc818e31dcc8ccccb9f89
sg4e <46875746+sg4e@users.noreply.github.com>
2026-04-21 03:51:21 +0100

    Configure test suite for CI/CD pipeline

--- server/src/test/java/moe/maika/fmteamhundo/api/ApiControllerIntegrationTest.java
+++ server/src/test/java/moe/maika/fmteamhundo/api/ApiControllerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,6 +28,7 @@ import moe.maika.fmteamhundo.service.ApiKeyService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ApiControllerIntegrationTest {
 
     @Autowired
