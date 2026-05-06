dd6c9d6cb3bddc259b2c9812b623ac77359dc6b5
sg4e <46875746+sg4e@users.noreply.github.com>
2026-05-06 04:37:59 +0100

    Remove quotation marks

--- middleware/scripts/run_FM_Sentinel.bat
+++ middleware/scripts/run_FM_Sentinel.bat
@@ -1,4 +1,4 @@
 @echo off
 FM_Sentinel.exe
-echo "FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server."
+echo FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server.
 pause
