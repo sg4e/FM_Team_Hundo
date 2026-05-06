1d00870d4402fb1561b07292fa19d25e56424cc2
sg4e <46875746+sg4e@users.noreply.github.com>
2026-05-06 02:23:57 +0100

    Add execution scripts

---
+++ middleware/scripts/run_FM_Sentinel.bat
@@ -0,0 +1,3 @@
+FM_Sentinel.exe
+echo "FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server."
+pause
---
+++ middleware/scripts/run_FM_Sentinel.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./FM_Sentinel
+echo "FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server."
+read -p "Press any key to continue..." -n1 -s
