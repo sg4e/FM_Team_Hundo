de6075a668d34ef4d7bce0484c7c479b990513d3
sg4e <46875746+sg4e@users.noreply.github.com>
2026-05-29 18:56:05 -0400

    Add middleware test mode (#3)

---
+++ middleware/scripts/TEST_FM_Sentinel.bat
@@ -0,0 +1,4 @@
+@echo off
+FM_Sentinel.exe --test
+echo FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server.
+pause
---
+++ middleware/scripts/TEST_FM_Sentinel.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./FM_Sentinel --test
+echo FM_Sentinel has closed. You are disconnected from the FM Team Hundo collection server.
+read -p "Press any key to continue..." -n1 -s
